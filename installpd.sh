5830bdf2d2c41489b6d0a94fe8bc5d12d2c581cc
eddwatts <edd@eddtech.co.uk>
2017-03-13 08:54:17 +0000

    Update installpd.sh

--- installpd.sh
+++ installpd.sh
@@ -1,7 +1,7 @@
 mkdir lipopi
 cd lipopi
-wget https://raw.githubusercontent.com/eddwatts/lipopi/master/shutdown.py
-wget https://raw.githubusercontent.com/eddwatts/lipopi/master/shutdown.service
+wget https://raw.githubusercontent.com/eddwatts/lipopi/master/powerdown.py
+wget https://raw.githubusercontent.com/eddwatts/lipopi/master/powerdown.service
 sudo chmod +x powerdown.py
 sudo cp powerdown.service /etc/systemd/system/.
 sudo systemctl enable powerdown.service
